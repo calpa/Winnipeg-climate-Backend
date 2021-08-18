e2d5a55442c1f291a57431e29261eba2768e472c
calpa <calpaliu@gmail.com>
2021-08-18 09:11:11 -0700

    feat: add cors

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from routes.weather import router as WeatherRouter
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(WeatherRouter, tags=["Weather"], prefix="/weather")
 
